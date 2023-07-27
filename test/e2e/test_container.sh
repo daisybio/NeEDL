8ba0bc46fc7f8a830808f78a12fc062d08c6ddc1
juli-p <63345753+juli-p@users.noreply.github.com>
2023-07-27 18:33:14 +0200

    added e2e test

--- test/e2e/test_container.sh
+++ test/e2e/test_container.sh
@@ -13,6 +13,13 @@ set -e
 user_id=$(id -u)
 group_id=$(id -g)
 
+# create temp output folder
+mkdir -p ./test_out
+out_dir=$(realpath ./test_out)
+
+echo "Temp output directory: $out_dir"
+echo "Working directory: $(pwd)"
+
 
 # ---- tests
 
@@ -23,5 +30,28 @@ docker run --user $user_id:$group_id "$1" /NeEDL/test/model/bin/calculate_scores
 docker run --user $user_id:$group_id "$1" /NeEDL/test/model/bin/convert_to_binary --help
 
 
+# test NeEDL quick start example with dummy dataset
+docker run --user $user_id:$group_id \
+    -v "./test/e2e/dummy_dataset.json:/mnt/in_0:rw,Z" \
+    -v "$out_dir:/mnt/out:rw,Z"
+    "$1" \
+    /NeEDL/test/model/bin/NeEDL \
+    --data-directory /NeEDL/data/ \
+    "--num-threads" "1" \
+    "--output-directory" "/mnt/out" \
+    "--input-format" "JSON_EPIGEN" \
+    "--input-path" "/mnt/in_0" \
+    "--phenotype" "DICHOTOMOUS" \
+    "--snp-annotate-dbSNP" \
+    "--network-BIOGRID" \
+    "--ms-seeding-routine" "RANDOM_CONNECTED" \
+    "--ms-rc-start-seeds" "5" \
+    "--ms-model" "PENETRANCE_NLL"
+
+
 # TODO: add more tests here
 # ...
+
+
+# remove temp folder
+rm -rf ./test_out
