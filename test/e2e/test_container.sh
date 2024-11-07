2a6ecd4a8af41446c07dc933736c75b80ddbf940
Julian Poschenrieder <juliposch@aol.com>
2024-11-07 14:00:41 +0100

    added epiJSON test, added platform configuration option to all run scripts

--- test/e2e/test_container.sh
+++ test/e2e/test_container.sh
@@ -47,7 +47,7 @@ docker run --user $user_id:$group_id "$1" /NeEDL/test/model/bin/realtime_scores 
 
 
 # select small dummy dataset
-dummy_dataset=$(realpath ./data/e2e_tests/dummy_dataset.json)
+dummy_dataset=$(realpath ./data/e2e_tests/dummy_dataset)
 
 # test NeEDL quick start example with dummy dataset
 python ./run/NeEDL.py \
@@ -57,7 +57,7 @@ python ./run/NeEDL.py \
     "--num-threads" "1" \
     "--output-directory" "$out_dir" \
     "--input-format" "JSON_EPIGEN" \
-    "--input-path" "$dummy_dataset" \
+    "--input-path" "$dummy_dataset.json" \
     "--phenotype" "DICHOTOMOUS" \
     "--snp-annotate-dbSNP" \
     "--network-BIOGRID" \
@@ -74,7 +74,7 @@ python ./run/NeEDL.py \
     "--num-threads" "1" \
     "--output-directory" "$out_dir" \
     "--input-format" "JSON_EPIGEN" \
-    "--input-path" "$dummy_dataset" \
+    "--input-path" "$dummy_dataset.json" \
     "--phenotype" "DICHOTOMOUS" \
     "--snp-annotate-eQTL" \
     "--network-BIOGRID" \
@@ -96,7 +96,7 @@ python ./run/NeEDL.py \
     "--num-threads" "1" \
     "--output-directory" "$out_dir" \
     "--input-format" "JSON_EPIGEN" \
-    "--input-path" "$dummy_dataset" \
+    "--input-path" "$dummy_dataset.json" \
     "--phenotype" "DICHOTOMOUS" \
     "--snp-annotate" "$annotation_file|yes|SNP|gene|\t|-1|-1" \
     "--network" "dummy_network|$network_file|yes|col1|col2|\t|-1|-1" \
@@ -104,6 +104,33 @@ python ./run/NeEDL.py \
     "--ms-rc-start-seeds" "5" \
     "--ms-model" "PENETRANCE_NLL"
  
+
+# epiJSON convert json --> all formats
+epijson_test_dir_1=$out_dir/"epiJSON_test_1"
+python ./run/epiJSON.py \
+    --container-image-name "$1" \
+    --docker-no-pulling \
+    $py_platform_str \
+    "--num-threads" "1" \
+    "--output-directory" "$epijson_test_dir_1" \
+    "--input-format" "JSON" \
+    "--input-file" "$dummy_dataset.json" \
+    "--phenotype" "DICHOTOMOUS" \
+    "--make-all-formats"
+
+cmp $epijson_test_dir_1/dataset.json ${dummy_dataset}_recreated.json
+cmp $epijson_test_dir_1/dataset.bim $dummy_dataset.bim
+cmp $epijson_test_dir_1/dataset.bed $dummy_dataset.bed
+cmp $epijson_test_dir_1/dataset.fam $dummy_dataset.fam
+cmp $epijson_test_dir_1/dataset.linden.cases $dummy_dataset.linden.cases
+cmp $epijson_test_dir_1/dataset.linden.controls $dummy_dataset.linden.controls
+cmp $epijson_test_dir_1/dataset.linden.loci $dummy_dataset.linden.loci
+cmp $epijson_test_dir_1/dataset.macoed $dummy_dataset.macoed
+cmp $epijson_test_dir_1/dataset.map $dummy_dataset.map
+cmp $epijson_test_dir_1/dataset.ped $dummy_dataset.ped
+cmp $epijson_test_dir_1/dataset.tfam $dummy_dataset.tfam
+cmp $epijson_test_dir_1/dataset.tped $dummy_dataset.tped
+cmp $epijson_test_dir_1/dataset.vcf $dummy_dataset.vcf
 
 
 # TODO: add more tests here
